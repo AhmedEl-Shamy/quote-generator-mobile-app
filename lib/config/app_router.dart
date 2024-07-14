a4ee52f7414d1373d6c635ab5f9b36a3d0ed6582
Ahmed Osama <ahmedosama14620@gmail.com>
2024-07-15 02:43:50 +0300

    add app router to project

---
+++ lib/config/app_router.dart
@@ -0,0 +1,21 @@
+import 'package:go_router/go_router.dart';
+import 'package:quote_generator_mobile_app/features/favorites/presentation/pages/favorites_page.dart';
+import 'package:quote_generator_mobile_app/features/home/presentation/pages/home_page.dart';
+
+abstract class AppRouter {
+  static const String homeRoute = '/home';
+  static const String favoritesRoute = '/favorites';
+
+  static final GoRouter router = GoRouter(
+    routes: [
+      GoRoute(
+        path: homeRoute,
+        builder: (context, state) => const HomePage(),
+      ),
+      GoRoute(
+        path: favoritesRoute,
+        builder: (context, state) => const FavoritesPage(),
+      ),
+    ],
+  );
+}
