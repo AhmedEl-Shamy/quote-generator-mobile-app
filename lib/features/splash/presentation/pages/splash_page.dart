4250bbde1f6558ef5c751e283b86797ab21bd661
Ahmed Osama <ahmedosama14620@gmail.com>
2024-07-15 02:53:15 +0300

    add splash feature

---
+++ lib/features/splash/presentation/pages/splash_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:quote_generator_mobile_app/core/widgets/custom_page.dart';
+
+import '../widgets/splash_page_body.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomPage(
+      body: SplashPageBody(),
+    );
+  }
+}
+
+
